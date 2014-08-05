9f08bb67a3f31f339690940d06a94019f4fb32ad
Rodrigo Buhler <rodrigo.buhler@ymail.com>
2014-08-05 00:09:07 -0300

    Pre beta 1.51 delivery
    All the functions were encapsulated inside the main function and the
    last result is returned.

--- R/biomoncore.R
+++ R/biomoncore.R
@@ -5,70 +5,70 @@
 # July 2014
 # Version 1.10
 # ***************************************
-#
+
 # **************************************
-# FUNCTIONS
+# MAIN METHOD
 # **************************************
-# Packages check
-# **************************************
-pck_chk <- function(){
-  # STRINGR
-  chk <- find.package("stringr")
-  if(is.na(chk)){
-    install.packages("stringr", dependencies=TRUE)
+biomon_run <- function(i_analysis) {
+  # **************************************
+  # METHOD - Packages check
+  # **************************************
+  pck_chk <- function(){
+    # STRINGR
+    chk <- find.package("stringr")
+    if(is.na(chk)){
+      install.packages("stringr", dependencies=TRUE)
+    }
+    require(stringr)
   }
-  require(stringr)
-}
+  # **************************************
+  # METHOD - Data load
+  # **************************************
+  data_load <- function(i_file, i_header){  
+    file   <- i_file
+    header <- i_header
+    #---
+    path   <- file
+    data   <- read.csv (file = path, header=header, stringsAsFactors = FALSE, row.names = 1)
+    #---
+    o_data <- data
+    return(o_data)
+  }  
+  # **************************************
+  # METHOD - Coerce
+  # **************************************
+  coerce <- function(i_type, i_variable){
+    type     <- i_type
+    variable <- i_variable
+    ret   <- c()
+    switch(type,
+           numeric={
+             ret <- as.numeric(variable)
+           },
+           double={
+             ret <- as.double(variable)
+           },
+           character={
+             ret <- as.character(variable)
+           },
+           logical={
+             ret <- as.logical(variable)
+           },
+           vector={
+             ret <- list(c(variable))
+           },
+           list={
+             ret <- list(variable)
+           },
+           ret <- variable
+    )
+    #
+    o_return <- ret
+    return(o_return)
+  }
 # **************************************
-# Data load
+# CONSTANTS
 # **************************************
-data_load <- function(i_file, i_header){  
-  file   <- i_file
-  header <- i_header
-#---
-  path   <- file
-  data   <- read.csv (file = path, header=header, stringsAsFactors = FALSE, row.names = 1)
-#---
-  o_data <- data
-  return(o_data)
-}
-# **************************************
-# Coertion of variable types
-# **************************************
-coerce <- function(i_type, i_variable){
- type     <- i_type
- variable <- i_variable
- ret   <- c()
- switch(type,
-        numeric={
-          ret <- as.numeric(variable)
-        },
-        double={
-          ret <- as.double(variable)
-        },
-        character={
-          ret <- as.character(variable)
-        },
-        logical={
-          ret <- as.logical(variable)
-        },
-        vector={
-          ret <- list(c(variable))
-        },
-        list={
-          ret <- list(variable)
-        },
-          ret <- variable
- )
-  #
-  o_return <- ret
-  return(o_return)
-}
-# **************************************
-# Execution of methods
-# **************************************
-execute <- function(i_methods) {
-  # CONSTANTS
   AT   <- "@"
   NUM  <- "numeric"
   DBL  <- "double"
@@ -77,12 +77,18 @@ execute <- function(i_methods) {
   VEC  <- "vector"
   LIS  <- "list"
   PARM_POINTER <- 3 # fist position of the parameters
+  # **************************************
+  # PRE LOAD CALLS
+  # **************************************
+    pck_chk() # Check for necessary packages
+  # **************************************
+  # MAIN BODY
+  # **************************************
+  analysis  <- i_analysis
+  techniques <- data_load(analysis, FALSE)
 # --  
-  methods  <- i_methods
-  analysis <- data_load(methods, FALSE)
-# --  
-  obs_size  <- length(analysis[[1]]) # Lines
-  var_size  <- length(analysis) # Columns
+  obs_size  <- length(techniques[[1]]) # Lines
+  var_size  <- length(techniques) # Columns
   result    <- list() # Execution results
 # Lines of methods
   for (x in 1: obs_size) { 
@@ -91,14 +97,14 @@ execute <- function(i_methods) {
     parameters <- list()
     param_idx  <- 0
 # Get the short text - 1st position    
-    shorttext  <- analysis[x,1]
+    shorttext  <- techniques[x,1]
 # Get the method     - 2nd position    
-    method     <- analysis[x,2]  
+    method     <- techniques[x,2]  
 # Columns of parameters
     for (i in PARM_POINTER: var_size) { 
 # Get the parameters - 3rd and others
-      typ  = str_trim(analysis[x,i])
-      var = str_trim(analysis[x,i+1])
+      typ  = str_trim(techniques[x,i])
+      var = str_trim(techniques[x,i+1])
 # Parameter is an already calculated value typ='@'
       if(typ == AT && !is.na(typ)){
         index <- coerce(DBL, var) # var is a position not a value
@@ -135,19 +141,19 @@ execute <- function(i_methods) {
       ret_lst <- list(shorttext, ret_val, ret_typ)
       result[x] <- list(ret_lst)
 # Print the current result
-print(c(x, ret_lst[[1]], "<-",ret_lst[[2]]), quote=FALSE)
+# print(c(x, ret_lst[[1]], "<-",ret_lst[[2]]), quote=FALSE)
     }
-print("end.")
+# print("end.")
+return(ret_val)
 }
 
 # ***************************************
 # MAIN
 # source("./R/biomoncore.R")
 # ***************************************
-pck_chk() # Check for necessary packages
-# l_methods <- "/Users/rodrigobuhler/Dev/R/biomon_core/Data/Mean.csv"
-l_methods <- "/Users/rodrigobuhler/Dev/R/biomon_core/Data/Median.csv"
-# l_methods <- "/Users/rodrigobuhler/Dev/R/biomon_core/Data/ReadTable.csv"
-# l_methods <- "/Users/rodrigobuhler/Dev/R/biomon_core/Data/PCA.csv"
-
-execute(l_methods)
+# l_analysis <- "/Users/rodrigobuhler/Dev/R/biomon_core/Data/Mean.csv"
+l_analysis <- "/Users/rodrigobuhler/Dev/R/biomon_core/Data/Median.csv"
+# l_analysis <- "/Users/rodrigobuhler/Dev/R/biomon_core/Data/ReadTable.csv"
+# l_analysis <- "/Users/rodrigobuhler/Dev/R/biomon_core/Data/PCA.csv"
+message <- biomon_run(l_analysis)
+print(message)
